cde88887d16fba887820a023b27a7150274f5a10
Ratan Kaliani <ratan.kaliani@gmail.com>
2017-11-29 13:34:22 -0500

    Practice Adding

---
+++ FinalWeatherApp/app/src/main/java/edu/tjhsst/a2019rkaliani/finalweatherapp/Function.java
@@ -0,0 +1,131 @@
+package edu.tjhsst.a2019rkaliani.finalweatherapp;
+
+
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Function{
+
+    // Project Created by Ferdousur Rahman Shajib
+    // www.androstock.com
+
+    private static final String OPEN_WEATHER_MAP_URL =
+            "http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric";
+
+    private static final String OPEN_WEATHER_MAP_API = "de0286d9457cd009765cdf16a0d79166";
+
+
+
+
+
+    public interface AsyncResponse {
+
+        void processFinish(String output1, String output2, String output3, String output4, String output5, String output6, String output7);
+    }
+
+
+
+
+
+    public static class placeIdTask extends AsyncTask<String, Void, JSONObject> {
+
+        public AsyncResponse delegate = null;
+
+        public placeIdTask(AsyncResponse asyncResponse) {
+            delegate = asyncResponse;
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+
+            JSONObject jsonWeather = null;
+            try {
+                jsonWeather = getWeatherJSON(params[0], params[1]);
+            } catch (Exception e) {
+                Log.d("Error", "Cannot process JSON results", e);
+            }
+
+
+            return jsonWeather;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if(json != null){
+                    JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+                    JSONObject main = json.getJSONObject("main");
+                    DateFormat df = DateFormat.getDateTimeInstance();
+
+
+                    String city = json.getString("name").toUpperCase(Locale.US) + ", " + json.getJSONObject("sys").getString("country");
+                    String description = details.getString("description").toUpperCase(Locale.US);
+                    String temperature = String.format("%.2f", main.getDouble("temp"))+ "°";
+                    String humidity = main.getString("humidity") + "%";
+                    String pressure = main.getString("pressure") + " hPa";
+                    String updatedOn = df.format(new Date(json.getLong("dt")*1000));
+
+
+                    delegate.processFinish(city, description, temperature, humidity, pressure, updatedOn, ""+ (json.getJSONObject("sys").getLong("sunrise") * 1000));
+
+                }
+            } catch (JSONException e) {
+                //Log.e(LOG_TAG, "Cannot process JSON results", e);
+            }
+
+
+
+        }
+    }
+
+
+
+
+
+
+    public static JSONObject getWeatherJSON(String lat, String lon){
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_MAP_URL, lat, lon));
+            HttpURLConnection connection =
+                    (HttpURLConnection)url.openConnection();
+
+            connection.addRequestProperty("x-api-key", OPEN_WEATHER_MAP_API);
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            StringBuffer json = new StringBuffer(1024);
+            String tmp="";
+            while((tmp=reader.readLine())!=null)
+                json.append(tmp).append("\n");
+            reader.close();
+
+            JSONObject data = new JSONObject(json.toString());
+
+            if(data.getInt("cod") != 200){
+                return null;
+            }
+
+            return data;
+        }catch(Exception e){
+            return null;
+        }
+    }
+
+
+
+
+}
+
---
+++ FinalWeatherApp/app/src/main/java/edu/tjhsst/a2019rkaliani/finalweatherapp/MainActivity.java
@@ -0,0 +1,60 @@
+package edu.tjhsst.a2019rkaliani.finalweatherapp;
+
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.widget.TextView;
+
+import com.androstock.myweatherapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView cityField, detailsField, currentTemperatureField, humidity_field, pressure_field, weatherIcon, updatedField;
+
+    Typeface weatherFont;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+
+
+
+        cityField = (TextView)findViewById(R.id.city_field);
+        updatedField = (TextView)findViewById(R.id.updated_field);
+        detailsField = (TextView)findViewById(R.id.details_field);
+        currentTemperatureField = (TextView)findViewById(R.id.current_temperature_field);
+        humidity_field = (TextView)findViewById(R.id.humidity_field);
+        pressure_field = (TextView)findViewById(R.id.pressure_field);
+        weatherIcon = (TextView)findViewById(R.id.weather_icon);
+
+
+
+
+        Function.placeIdTask asyncTask =new Function.placeIdTask(new Function.AsyncResponse() {
+            
+
+            public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn, String sun_rise) {
+
+                cityField.setText(weather_city);
+                updatedField.setText(weather_updatedOn);
+                detailsField.setText(weather_description);
+                currentTemperatureField.setText(weather_temperature);
+                humidity_field.setText("Humidity: "+weather_humidity);
+                pressure_field.setText("Pressure: "+weather_pressure);
+
+
+            }
+        });
+        asyncTask.execute("41.8781", "-87.6298"); //  asyncTask.execute("Latitude", "Longitude")
+
+
+
+    }
+
+
+}
